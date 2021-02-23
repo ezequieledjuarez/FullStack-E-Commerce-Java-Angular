79b97920599cd315707bc6433dff19ac5fac2cc0
EzzeGuason <ezequieledjuarez@gmail.com>
2021-02-23 12:28:42 -0300

    Conexion a BD, creacion de DAO

--- DB/db-scripts/01-create-user.sql
+++ DB/db-scripts/01-create-user.sql
@@ -1,15 +1,9 @@
+DROP USER if exists 'ecommerceapp'@'localhost' ;
+ 
+ 
+-- Now create user with proper privs
 CREATE USER 'ecommerceapp'@'localhost' IDENTIFIED BY 'ecommerceapp';
-
+ 
 GRANT ALL PRIVILEGES ON * . * TO 'ecommerceapp'@'localhost';
-
-#
-# Starting with MySQL 8.0.4, the MySQL team changed the 
-# default authentication plugin for MySQL server 
-# from mysql_native_password to caching_sha2_password.
-#
-# The command below will make the appropriate updates for your user account.
-#
-# See the MySQL Reference Manual for details: 
-# https://dev.mysql.com/doc/refman/8.0/en/caching-sha2-pluggable-authentication.html
-#
+ 
 ALTER USER 'ecommerceapp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ecommerceapp';
