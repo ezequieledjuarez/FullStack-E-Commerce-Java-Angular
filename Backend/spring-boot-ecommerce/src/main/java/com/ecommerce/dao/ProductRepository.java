e31252ba57385943082ee3f0baf8f3373de9a051
EzzeGuason <ezequieledjuarez@gmail.com>
2021-02-25 10:36:50 -0300

    Integracion de FrontEnd con la Base de Datos, CrossOver on, primera vistsa

--- Backend/spring-boot-ecommerce/src/main/java/com/ecommerce/dao/ProductRepository.java
+++ Backend/spring-boot-ecommerce/src/main/java/com/ecommerce/dao/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecommerce.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
